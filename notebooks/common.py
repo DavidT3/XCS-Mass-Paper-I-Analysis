66f726ba7949a7e1220e44823495c4c3183e53fa
DavidT3 <dt237@sussex.ac.uk>
2023-05-29 20:09:40 +0100

    Added rad uncertainties to GM calculatio for XXL - in the actual notebook rather than the experimenty one I was using before. Also updated common.py's comparison plotter to accept a colour

--- notebooks/common.py
+++ notebooks/common.py
@@ -61,7 +61,7 @@ def find_lims(x_dat, y_dat, buffer=0.1):
 def direct_comparison_plot(xdat: Union[Quantity, List[Quantity]], ydat: Union[Quantity, List[Quantity]], xlabs, ylabs,
                            samp_names, figsize, xscale: Union[str, list] = 'log', yscale: Union[str, list] = 'log', 
                            buffer=0.1, sublabel_fsize=14, savepath=None, low_lim: Quantity = None, 
-                           upp_lim: Quantity = None):
+                           upp_lim: Quantity = None, dat_colour: str = 'black'):
     
     if len(xdat) != len(ydat):
         raise ValueError("The input data are not the same length")
@@ -125,10 +125,10 @@ def direct_comparison_plot(xdat: Union[Quantity, List[Quantity]], ydat: Union[Qu
         cur_y = ydat[ax_ind]
         cur_name = samp_names[ax_ind]
         if cur_x.ndim == 1:
-            plt.plot(cur_x, cur_y, 'x', color='k', label=cur_name)
+            plt.plot(cur_x, cur_y, 'x', color=dat_colour, label=cur_name)
         else:
             plt.errorbar(cur_x[:, 0].value, cur_y[:, 0].value, xerr=cur_x[:, 1:].T.value, 
-                         yerr=cur_y[:, 1:].T.value, fmt="kx", capsize=2, label=cur_name)
+                             yerr=cur_y[:, 1:].T.value, color=dat_colour, fmt="x", capsize=2, label=cur_name)
         
         # Setting axis limits
         plt.xlim(lims)
